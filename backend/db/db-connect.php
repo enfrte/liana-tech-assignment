73ae74dea0a1a3ff18bd17944dabf7d65f5a707f
enfrte <enfrte@gmail.com>
2019-11-26 11:45:18 +0200

    init and finished

--- backend/db/db-connect.php
+++ backend/db/db-connect.php
@@ -1,14 +1,18 @@
 <?php
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "liana_demo";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $handler = new PDO('mysql:host='.$servername.';dbname='.$dbname, $username, $password);
+    
+    $handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Couldn't connect to database");
 }
+
+
 
 ?>
--- backend/newsletter-subscribe.php
+++ backend/newsletter-subscribe.php
@@ -4,13 +4,22 @@ require '../backend/db/db-connect.php';
 
 $email = $_POST['email'];
 
-$sql = "INSERT INTO email_subscriber (email)
-VALUES ('".$email."')";
+try {
+    $sql = "INSERT INTO email_subscriber (email) VALUES (:email)";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Thanks for your subscription!";
-} else {
-    echo "Error: " . $conn->error;
+    $query = $handler->prepare($sql);
+
+    $query->execute(array(
+        'email' => $email
+    ));
+    echo 'Successfully  registered!';
+} catch (PDOException $e) {
+    // 23000 = duplicate entry
+    if ($query->errorCode() === '23000') {
+        die('Email already registered.'); // user friendly message
+    }
+    // other errors
+    die('Error: '.$e->getMessage());
 }
 
 ?>
